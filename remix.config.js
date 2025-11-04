9130a9dceb49f791651bc5b857315cf169201831
Murali1801 <muralidharacharya7@gmail.com>
2025-11-04 21:10:30 +0530

    package

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,4 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
-  serverBuildPath: "build/server/index.js",
-  serverModuleFormat: "esm",
-  serverPlatform: "node",
 };
